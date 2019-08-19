a1b3b7438fdbf97f19c3513a6d47e8980e466ad5
marciodeveloper <marciodeveloper@outlook.com>
2019-08-19 19:39:56 -0300

    Definindo Rotas

---
+++ src/app/admin/admin-produtos/admin-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-produtos',
+  templateUrl: './admin-produtos.component.html',
+  styleUrls: ['./admin-produtos.component.css']
+})
+export class AdminProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,50 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+//Definindo Rotas
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProdutosComponent } from './admin/admin-produtos/admin-produtos.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    ProdutosComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProdutosComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     //Configurando o Firebase
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent}, 
+      {path: 'produtos', component: ProdutosComponent},
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+      {path: 'check-out', component: CheckOutComponent},
+      {path: 'order-success', component: OrderSuccessComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'admin/produtos', component: AdminProdutosComponent},
+      {path: 'admin/orders', component: AdminOrdersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
